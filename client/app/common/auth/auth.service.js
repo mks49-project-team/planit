7b3b18823e30226074b785f428715331f650858c
oliv3rwang <oliv3r.wang@gmail.com>
2016-10-18 22:43:51 -0700

    [feature] Add crypto feature

--- client/app/common/auth/auth.service.js
+++ client/app/common/auth/auth.service.js
@@ -1,3 +1,4 @@
+var crypto = require('crypto');
 (function() {
   'use strict';
 
@@ -13,3 +14,13 @@
 
 
 })
+
+var createEndpoint = function() {
+  crypto.randomBytes(10, function(error, buf) {
+    if(err) {
+      console.error(err)
+    } else {
+      console.log("This is the crypto", buf)
+    }
+  })
+}
