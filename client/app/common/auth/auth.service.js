7fb3885d2b81be38ef058c4c5939883e86a88fa4
oliv3rwang <oliv3r.wang@gmail.com>
2016-10-18 21:57:59 -0700

    [setup] Set file structure

---
+++ client/app/common/auth/auth.service.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module(app.auth)
+    .factory('appService', appService);
+
+  authService.$inject = ['$http'];
+
+  function authService($http) {
+
+  }
+
+
+})
