0ff8bffaae791d830d9a87fd975e4ff3d2436b23
Justin Biele <justin.biele@gmail.com>
2016-10-19 16:38:03 -0700

    [add] Return promise from yelp api

--- server/helpers/activityHelper.js
+++ server/helpers/activityHelper.js
@@ -3,31 +3,37 @@ var Yelp = require('yelp');
 
 var yelpQuery = new Yelp ({
 
-  consumer_key: process.env.consumer_key,
-  consumer_secret: process.env.consumer_secret,
-  token: process.env.token,
-  token_secret: process.env.token_secret
+  consumer_key: 'AokJ1I73m9ZXRKsQwAFk3w',
+  consumer_secret: 'RtuBaAZctDIRcvTKWBphtGeXVG0',
+  token: 'L5Q9dkCBNNr-GwZDl81mliC8z5OtWJjr',
+  token_secret: '5UIuf3CH89z_ODFJsfQA5RDoFLQ'
 
 })
 
 
-yelpQuery.search({ location: req.query })
-  .then(function(data) {
-    data.businesses.forEach(function(business) {
-      var businessEntry = {
-        id: business.id,
-        name: business.name,
-        rating: business.rating,
-        stars: business.rating_img_url_small,
-        categories: business.categories,
-        address: business.location.dispaly_address,
-        image: business.image_url,
-        description: business.snippet_text
+function yelpSearch(locationSearch) {
+  return yelpQuery.search({location: locationSearch})
+    .then(function(data) {
+      var businessEntry = [];
+      console.log('this is the data baybay')
+      data.businesses.forEach(function(business) {
+        businessEntry.push({
+          name: business.name,
+          rating: business.rating,
+          stars: business.rating_img_url_small,
+          address: business.location.display_address,
+          image: business.image_url,
+          description: business.snippet_text
 
-      };
-      console.log(businessEntry)
+        });
+        // console.log("this is a business entry", businessEntry)
+      })
+      return businessEntry;
     })
-  })
-  .catch(function(err) {
-    console.log('There was an error: ', err)
-  })
+    // .catch(function(err) {
+    //   console.log('There was an error: ', err)
+    // })
+}
+module.exports = {
+  yelpSearch: yelpSearch
+}
--- server/models/possibleActivities.js
+++ server/models/possibleActivities.js
@@ -3,6 +3,8 @@ var Sequelize = require('sequelize');
 module.exports = function(db) {
   var PossibleActivities = db.define('possibleActivities', {
     name: {type: Sequelize.STRING},
+    rating:{type: Sequelize.STRING},
+    stars:{type: Sequelize.STRING},
     picture: {type: Sequelize.STRING},
     description: {type: Sequelize.STRING},
     address: {type: Sequelize.STRING}
