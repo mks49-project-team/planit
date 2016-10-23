ae56c7d48718ccda247e30daf43bcaf659f1acb6
Dianne Le <diannefle@gmail.com>
2016-10-22 19:59:25 -0700

    [refactor] Refactor itinerary and add comments

--- client/app/components/itinerary/itinerary.controller.js
+++ client/app/components/itinerary/itinerary.controller.js
@@ -14,30 +14,28 @@
     vm.postSavedActivity = postSavedActivity;
     vm.deleteSelectedActivity = deleteSelectedActivity;
     vm.uuid;
-    
-    $scope.$on('selectedActivityChange', function(event, args) {
-      console.log('change detected', event);
-      postSavedActivity(args.val);
-      vm.getSavedActivities(vm.uuid);
-    });
+
+    /* *
+    * ItineraryController
+    *   - listens for a change in ParentController's uuid and selectedActivity values
+    *   - gets the saved activities from /api/itinerary for the trip with that uuid
+    *   - deletes the selectedActivity from possibleActivities
+    * */
 
     $scope.$on('uuidChange', function(event, args) {
-      // console.log('change detected', event, args.val);
       vm.uuid = args.val;
-      // console.log(vm.uuid);
-      // setTimeout(function() {
-      console.log('args.val', args.val);
-        vm.getSavedActivities(args.val);
-      // }, 1000);
+      vm.getSavedActivities(args.val);
     });
 
-    vm.getSavedActivities(vm.uuid);
+    $scope.$on('selectedActivityChange', function(event, args) {
+      vm.postSavedActivity(args.val);
+      vm.getSavedActivities(vm.uuid);
+    });
 
     function getSavedActivities(uuid) {
       return itineraryService.getSavedActivities(uuid)
         .then(function(data) {
           vm.savedActivities = data;
-          console.log('got savedActivities', data);
         })
         .catch(function(err) {
           console.log('err in getSavedActivities', err);
@@ -47,7 +45,7 @@
     function postSavedActivity(activity) {
       return itineraryService.postSavedActivity(activity)
         .then(function(data) {
-          console.log('saved activity', data);
+          console.log('saved this activity', data);
         })
         .catch(function(err) {
           console.log('err in postSavedActivity', err);
@@ -64,15 +62,12 @@
         });
     }
 
-    function init() {
-      console.log('inside itineraryController init');
-      vm.getSavedActivities();
-    }
-
-    init();
+    /* *
+    * There is a setTimeout here because we need to retrieve the uuid value
+    * before getting a trip's saved activities.
+    * */
 
     setTimeout(function() {
-      console.log(vm.uuid, 'this is activities.controller');
       vm.getSavedActivities(vm.uuid);
     }, 1500);
   }
