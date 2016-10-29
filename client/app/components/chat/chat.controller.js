e3eabe5a198f5ba15ab98b9015e3c5c5fd0ad908
Lucy Ji <lucyji1000@gmail.com>
2016-10-29 09:43:05 -0700

    [cleanup] clean up chat code

--- client/app/components/chat/chat.controller.js
+++ client/app/components/chat/chat.controller.js
@@ -24,40 +24,40 @@
         var socket = io.connect($window.location.origin);
 
         socket.on('connect', function(){
+          // get previous chat history if it exists
+          chatService.getChat(uuid)
+            .then(function(chats){
+              if(chats.length > 0){
+                vm.messages = chats;
+                updateScroll();
+              }
+            });
 
-        // get previous chat history if it exists
-        chatService.getChat(uuid)
-          .then(function(chats){
-            if(chats.length > 0){
-              vm.messages = chats;
+          socket.emit('new user', {
+            username: $window.localStorage.username,
+            room: uuid
+          });
+
+          socket.on('message created', function(data){
+            $scope.$apply(function(){
+              vm.messages.push(data);
               updateScroll();
-            }
+            });
           });
 
-        socket.emit('new user', {
-          username: $window.localStorage.username,
-          room: uuid
-        });
-
-        socket.on('message created', function(data){
-          $scope.$apply(function(){
-            vm.messages.push(data);
-            updateScroll();
+          socket.on('is typing', function(data){
+            $scope.$apply(function(){
+              vm.otherUserTyping = true;
+              vm.typingUser = data.username;
+            });
           });
-        });
 
-        socket.on('is typing', function(data){
-          $scope.$apply(function(){
-            vm.otherUserTyping = true;
-            vm.typingUser = data.username;
+          socket.on('stopped typing', function(data){
+            $scope.$apply(function(){
+              vm.otherUserTyping = false;
+            });
           });
-        });
 
-        socket.on('stopped typing', function(data){
-          $scope.$apply(function(){
-            vm.otherUserTyping = false;
-          });
-        });
       });
 
       vm.sendMsg = function() {
